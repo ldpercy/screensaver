7e4ca6966b74ef2f5729f61678f41232d2885039
Paul Dale <ldpercy@gmail.com>
2026-04-01 12:40:13 +1100

    Add movement for rectangle

--- screensaver/rect/module.js
+++ screensaver/rect/module.js
@@ -1,13 +1,15 @@
 
 import { Screensaver } from "../screensaver.js";
+import * as maths from "../../[html-common]/module/Maths.js";
 
 
 
 console.log('rect module');	// this only runs the _first_ time the module is loaded - not sure what the stipulations around that are though, whether it's possible to unload etc
 
 
-const c = document.createElementNS('http://www.w3.org/2000/svg','circle');
 
+const ssg = document.getElementById('screensaver-group');
+const r = document.createElementNS('http://www.w3.org/2000/svg','rect');
 
 class RectScreensaver extends Screensaver {
 
@@ -19,8 +21,44 @@ class RectScreensaver extends Screensaver {
 
 	init() {
 		console.log('RectScreensaver init');
-		document.getElementById('screensaver-group').innerHTML = '<rect x="-400" y="-300" width="600" height="500"></rect>';
+		//document.getElementById('screensaver-group').innerHTML = '<rect x="-400" y="-300" width="600" height="500"></rect>';
+		this.moveRect();
+		ssg.appendChild(r);
 	}
+
+
+
+	start() {
+		this.intervalId = setInterval(
+			()=> { this.moveRect() },
+			1000
+		);
+		console.log(this.intervalId);
+	}
+
+
+	stop() {
+		console.log('circle stop:', this.intervalId);
+		clearInterval(this.intervalId);
+		this.intervalId = undefined;
+	}
+
+
+
+	moveRect() {
+
+		const newX = maths.getRandomIntInclusive(-100,100);
+		const newY = maths.getRandomIntInclusive(-100,100);
+		const newWidth = maths.getRandomIntInclusive(10,1000);
+		const newHeight = maths.getRandomIntInclusive(10,1000);
+
+		r.setAttribute('x', `${newX}vw`);
+		r.setAttribute('y', `${newY}vh`);
+		r.setAttribute('width', `${newWidth}px`);
+		r.setAttribute('height', `${newHeight}px`);
+	}
+
+
 
 
 }/* RectScreensaver */
